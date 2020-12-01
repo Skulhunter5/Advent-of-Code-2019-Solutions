142acb8ea49eb51ac68d8423ef5b37e640fff728
Skulhunter5 <62426864+Skulhunter5@users.noreply.github.com>
2020-12-01 21:50:15 +0100

    Created solution

---
+++ day 2/part_one.py
@@ -0,0 +1,24 @@
+with open("res/program1", 'r') as file:
+	memory = list(map(int, file.readlines()[0].split(",")))
+
+memory[1] = 12
+memory[2] = 2
+
+instructionPointer = 0
+while(instructionPointer < len(memory)):
+	opcode = memory[instructionPointer]
+	if(opcode == 1):
+		param0 = memory[instructionPointer+1]
+		param1 = memory[instructionPointer+2]
+		param2 = memory[instructionPointer+3]
+		memory[param2] = memory[param0] + memory[param1]
+		instructionPointer += 4
+	elif(opcode == 2):
+		param0 = memory[instructionPointer+1]
+		param1 = memory[instructionPointer+2]
+		param2 = memory[instructionPointer+3]
+		memory[param2] = memory[param0] * memory[param1]
+		instructionPointer += 4
+	elif(opcode == 99):
+		print(memory)
+		exit()
---
+++ day 2/part_two.py
@@ -0,0 +1,29 @@
+with open("res/program1", 'r') as file:
+	program = list(map(int, file.readlines()[0].split(",")))
+
+for noun in range(100):
+	for verb in range(100):
+		memory = program.copy()
+		memory[1] = noun
+		memory[2] = verb
+
+		instructionPointer = 0
+		while(instructionPointer < len(memory)):
+			opcode = memory[instructionPointer]
+			if(opcode == 1):
+				param0 = memory[instructionPointer+1]
+				param1 = memory[instructionPointer+2]
+				param2 = memory[instructionPointer+3]
+				memory[param2] = memory[param0] + memory[param1]
+				instructionPointer += 4
+			elif(opcode == 2):
+				param0 = memory[instructionPointer+1]
+				param1 = memory[instructionPointer+2]
+				param2 = memory[instructionPointer+3]
+				memory[param2] = memory[param0] * memory[param1]
+				instructionPointer += 4
+			elif(opcode == 99):
+				if(memory[0] == 19690720):
+					print(i, j)
+					print(memory)
+				break
